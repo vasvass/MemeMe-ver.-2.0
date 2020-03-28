34c5639c49401c204b8014a1eb49a695ff4c77ba
Vassilis Vassileiadis <vasvass@gmail.com>
2020-03-28 20:29:17 +0200

    Initial Commit

---
+++ MemeMe ver. 2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe ver. 2.0
+//
+//  Created by Vassileios Vassileiades on 28/3/20.
+//  Copyright © 2020 Vassileios Vassileiades. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
